6bec20717cffe6579be685296f86684ac0fa2ac0
Arran Dyer <arran549@gmail.com>
2018-06-17 20:50:05 +0100

    remove unused styles in app.js

--- App.js
+++ App.js
@@ -28,24 +28,3 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  btn: {
-    backgroundColor: '#E53224',
-    padding: 10,
-    paddingLeft: 50,
-    paddingRight: 50,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 5
-  },
-  btnText: {
-    color: '#fff'
-  }
-});
-
-
