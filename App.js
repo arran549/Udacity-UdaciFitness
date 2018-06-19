e0658c2d2f1692a5b588d5449b37723777283698
Arran Dyer <arran549@gmail.com>
2018-06-19 08:01:14 +0100

    History view - still in progress (end of video 6:53)

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { View, StyleSheet } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
 
@@ -19,7 +20,7 @@ export default class App extends React.Component {
     return (
       <Provider store={this.store}>
         <View style={{flex: 1}} >
-          <AddEntry />
+          <History />
         </View>
       </Provider>
     );
